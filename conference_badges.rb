8c1494f1117aa664f0eb205b82322fd0c02b401f
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-20 21:33:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,15 @@
 # Write your code here.
 
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
 def batch_badge_creator(array)
   array.collect { |x|
-    ""
+    badge_maker(x)
+  }
+end
+
+def assign_rooms(array)
+  
 end
